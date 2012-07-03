b09ee9f3c1f12ed11d3ef4754c9b260d96167696
Nicholas Watson <nwwatson@gmail.com>
2012-07-03 08:38:28 -0400

    added initializer to allow decorators

--- lib/appstore/engine.rb
+++ lib/appstore/engine.rb
@@ -1,5 +1,14 @@
 module Appstore
   class Engine < ::Rails::Engine
     isolate_namespace Appstore
+    
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
+    
   end
 end
